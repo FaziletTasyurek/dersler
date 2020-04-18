ecd0b5bff12d4a8599eb7d900b0882b9b9c108e8
FaziletTasyurek <fazilett8@gmail.com>
2020-04-18 11:11:12 +0300

    ders1yeni

---
+++ ders2.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a string = "değişken 1"
+	var b = 50
+	var c bool = true
+
+	var x = 42
+	var y = 34
+
+	fmt.Println(a)
+
+	fmt.Println(b)
+
+	fmt.Println(c)
+
+	fmt.Println("x + y")
+
+	fmt.Println(x + y)
+
+}
