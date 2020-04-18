ee651cc58dc33ca10d4e2762b5cf766748cf6672
FaziletTasyurek <fazilett8@gmail.com>
2020-04-18 11:32:36 +0300

    ders4yeni

---
+++ ders4.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+	/*
+		for i := 1; i < 10; i++ {
+
+			fmt.Println(i)
+		}
+	*/
+	/*
+		i := 1
+		for i < 10 {
+			fmt.Println(i)
+			i++
+		}
+	*/
+	/* Sonsuz döngü
+		i := 1
+		for {
+			fmt.Println(i)
+			i++
+	     break
+		}
+	*/
+}
