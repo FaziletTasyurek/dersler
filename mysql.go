985afc0aed89667b0ce7b5cd52362a0c1f8f343a
FaziletTasyurek <fazilett8@gmail.com>
2020-04-20 22:53:14 +0300

    mysql

---
+++ mysql.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type personeller struct {
+	Name    string
+	Surname string
+	Age     int
+	Salary  int
+}
+
+func main() {
+	db, err := gorm.Open("mysql", "root:root@/deneme?charset=utf8&parseTime=True&loc=Local")
+	fmt.Print(err)
+	db.AutoMigrate(&personeller{})
+	//db.DropTableIfExists(&personeller{})
+	defer db.Close()
+}
